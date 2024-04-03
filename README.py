b4ddb9b564664855308d0605d89d02ac0133b02b
aellis18 <157083850+aellis18@users.noreply.github.com>
2024-04-02 21:39:07 -0500

    final project

---
+++ README.py
@@ -0,0 +1,13 @@
+title: Swindler
+team members: Avery, Gaby and Chanelle
+a team name, this also should be the name of the team on GiHub Classroom: Steminists
+a short description of the project: Mimic of the snake game- snake starts small, as you get “apples” it gets longer and you try to win by filling up the entire screen
+why you choose this project: We all used to play the snake game in middle school
+the inspiration behind the project: The Snake Game
+a rough implementation and responsibility plan, ie, what needs to be accomplished, who will do what when.  Think about the list of deliverables when building the plan.
+Avery main driver
+Gaby visuals
+Chanelle coding concepts
+To accomplish: have a “snake” that gains length everytime it hit/”eats” an “apple”
+Game over when snake hits a wall or itself
+Display score (length) at the end
